b28b22ea8c107421233194c9e5a13e4d955d61f2
gislane.nunes <gislane_nunes@yahoo.com.br>
2022-07-14 15:54:00 -0300

    tentando criar a página de cadastro

--- googlekeep/urls.py
+++ googlekeep/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
     path('notas/', include('listanotas.urls')),
+    path('usuarios/', include('usuarios.urls')),
 ]
